d009d875411d8c1a4f5102dc4190b1a54fe1a859
skomaragiri <71363974+skomaragiri@users.noreply.github.com>
2022-03-09 06:41:07 -0800

    Update sound_synth_project.cpp

--- sound_synth_project/sound_synth_project/sound_synth_project.cpp
+++ sound_synth_project/sound_synth_project/sound_synth_project.cpp
@@ -37,7 +37,7 @@ int main()
 		"|  Z  |  X  |  C  |  V  |  B  |  N  |  M  |  ,  |  .  |  /  |" << endl <<
 		"|_____|_____|_____|_____|_____|_____|_____|_____|_____|_____|" << endl << endl;
 
-	// Create sound machine!!
+	// Create sound machine
 	NoiseMaker<short> sound(devices[0], 44100, 1, 8, 512);
 
 	// Link noise function with sound machine
@@ -79,14 +79,3 @@ int main()
 
 	return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
